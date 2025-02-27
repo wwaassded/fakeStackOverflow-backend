f141ac9426f7512e594e4105e41a5f3884a29149
Bangjing Yi <135570318+wwaassded@users.noreply.github.com>
2025-02-27 11:56:45 +0800

    init

---
+++ src/main/java/com/what/spring/FakeStackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.what.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStackOverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeStackOverflowApplication.class, args);
+    }
+
+}
