7b90aae8438233232f62ced70760785e9848bbc1
Bangjing Yi <135570318+wwaassded@users.noreply.github.com>
2025-03-10 21:34:27 +0800

    fix some problem in session

--- src/main/java/com/what/spring/config/UtilConfig.java
+++ src/main/java/com/what/spring/config/UtilConfig.java
@@ -1,15 +1,24 @@
 package com.what.spring.config;
 
+import com.what.spring.pojo.user.SessionCounter;
 import com.what.spring.pojo.user.UserSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Configuration
 public class UtilConfig {
     @Bean(name = "sessionCache")
-    public ConcurrentHashMap<String, UserSession> sessionCache() {
+    public ConcurrentHashMap<String, SessionCounter> sessionCache() {
+        //TODO 启动式可以考虑将多个用户session加载进来
+        return new ConcurrentHashMap<>();
+    }
+
+    @Bean(name = "ReentrantLockCache")
+    public ConcurrentHashMap<String, ReentrantLock> reentrantLockCache() {
         //TODO 启动式可以考虑将多个用户session加载进来
         return new ConcurrentHashMap<>();
     }
--- src/main/java/com/what/spring/interceptor/SessionPermissionsInterceptor.java
+++ src/main/java/com/what/spring/interceptor/SessionPermissionsInterceptor.java
@@ -1,6 +1,7 @@
 package com.what.spring.interceptor;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.what.spring.pojo.user.SessionCounter;
 import com.what.spring.pojo.user.UserSession;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.Cookie;
@@ -13,13 +14,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 @SuppressWarnings("NullableProblems")
 @Component("sessionPermissionsInterceptor")
 public class SessionPermissionsInterceptor implements HandlerInterceptor {
 
     @Resource(name = "sessionCache")
-    private ConcurrentHashMap<String, UserSession> sessionHashMap;
+    private ConcurrentHashMap<String, SessionCounter> sessionConcurrentHashMap;
+
+    @Resource(name = "ReentrantLockCache")
+    private ConcurrentHashMap<String, ReentrantLock> reentrantLockConcurrentHashMap;
 
     @Resource(name = "myRedisTemplate")
     private RedisTemplate<String, Object> redisTemplate;
@@ -39,20 +44,28 @@ public class SessionPermissionsInterceptor implements HandlerInterceptor {
         for (var cookie : cookies) {
             if (cookie.getName().equals("sessionId")) {
                 String sessionId = cookie.getValue();
-                if (sessionHashMap.containsKey(sessionId)) {
-                    request.setAttribute("userSession", sessionHashMap.get(sessionId));
-                } else {
-                    String userSessionJson = (String) redisTemplate.opsForValue().get(sessionId);
-                    if (userSessionJson == null) {
-                        LOG.info("拦截器失败");
-                        return false;
+                ReentrantLock reentrantLock = reentrantLockConcurrentHashMap.computeIfAbsent(sessionId, k -> new ReentrantLock());
+                reentrantLock.lock();
+                try {
+                    if (sessionConcurrentHashMap.containsKey(sessionId)) {
+                        SessionCounter sessionCounter = sessionConcurrentHashMap.get(sessionId);
+                        sessionCounter.click();
+                        request.setAttribute("userSession", sessionCounter.getUserSession());
+                    } else {
+                        String userSessionJson = (String) redisTemplate.opsForValue().get(sessionId);
+                        if (userSessionJson == null) {
+                            LOG.info("拦截器失败");
+                            return false;
+                        }
+                        UserSession userSession = objectMapper.readValue(userSessionJson, UserSession.class);
+                        sessionConcurrentHashMap.put(sessionId, new SessionCounter(userSession, 1));
+                        request.setAttribute("userSession", userSession);
                     }
-                    UserSession userSession = objectMapper.readValue(userSessionJson, UserSession.class);
-                    sessionHashMap.put(sessionId, userSession);
-                    request.setAttribute("userSession", userSession);
+                    LOG.info("拦截器成功喽");
+                    return true;
+                } finally {
+                    reentrantLock.unlock();
                 }
-                LOG.info("拦截器成功喽");
-                return true;
             }
         }
         LOG.info("拦截器失败");
